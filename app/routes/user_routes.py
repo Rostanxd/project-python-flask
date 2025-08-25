e8d58faf8fb59bfc8e63fe38a4e85495fc542bad
rostan <rostanxd@gmail.com>
2025-08-24 22:13:02 -0500

    issue/role-entity - Add `/user/roles` endpoint to update user roles and validate inputs

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,11 +1,13 @@
 from flask import Blueprint, jsonify, request
 from werkzeug.exceptions import NotFound, BadRequest
+
+from ..models import Role
 from ..services.user_service import (
     create_user,
     check_password,
     toggle_status,
     get_user_by_email,
-    get_all_users,
+    get_all_users, user_update_roles,
 )
 
 user_bp = Blueprint("user_bp", __name__)
@@ -80,3 +82,33 @@ def get_user_details():
         ),
         200,
     )
+
+@user_bp.route("/user/roles", methods=["PATCH"])
+def update_user_roles():
+    data = request.get_json()
+    email = data.get("email")
+    roles = data.get("roles")
+    
+    # Check if the body payload is valid
+    if not email or not roles:
+        return jsonify({"error": "email and roles are required"}), 400
+    if not isinstance(roles, list) or len(roles) == 0:
+        return jsonify({"error": "roles must be a non-empty array of numbers"}), 400
+    
+    user = get_user_by_email(email=email)
+    if not user:
+        return jsonify({"error": "User not found"}), 404
+
+    try:
+        data = user_update_roles(user_id=user.id, roles=roles)
+        return jsonify({
+            "id": user.id,
+            "email": user.email,
+            "username": user.username,
+            "roles": data,
+        }), 200
+    except ValueError as ve:
+        return jsonify({"error": str(ve)}), 400
+    except Exception as e:
+        print("Unexpected error: {}".format(str(e)))
+        return jsonify({"error": "Unexpected Error"}), 500
