f0f376c1f67f1f5cf56b2957994a9905e7b5868d
rostan <rostanxd@gmail.com>
2025-08-25 20:39:50 -0500

    issue/profile-route - Add tests for `/profiles` and `/profiles/<profile_id>` endpoints

--- tests/test_profiles.py
+++ tests/test_profiles.py
@@ -1,6 +1,25 @@
+from tests.fixtures.profiles import TEST_PROFILE
+
+
 def test_all_profiles(client, create_test_profile):
     # Send a GET request to get all the profiles in the system
     response = client.get("/profiles")
 
     # Asser that the response is 200 (OK)
     assert response.status_code == 200
+
+
+def test_fetch_profile(client, create_test_profile):
+    # Send a GET request to get all the profiles in the system
+    response = client.get("/profiles/{}".format(create_test_profile.id))
+
+    # Asser that the response is 200 (OK)
+    assert response.status_code == 200
+
+
+def test_profile_not_found(client):
+    # Send a GET request to get all the profiles in the system
+    response = client.get("/profiles/{}".format(TEST_PROFILE.get('id')))
+
+    # Asser that the response is 200 (OK)
+    assert response.status_code == 404
