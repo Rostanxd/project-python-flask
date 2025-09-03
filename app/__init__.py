650d1cabd58891d212370739e276b6658e6bd539
rostan <rostanxd@gmail.com>
2025-09-02 21:49:40 -0500

    feature/implement-swagger - Add Swagger integration for API documentation

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,34 @@
 from flask import Flask
+from flasgger import Swagger
 from .extensions import db, migrate, bcrypt
 from .config import Config
 from .routes import register_blueprints
+
+
+swagger_template = {
+    "swagger": "2.0",
+    "info": {
+        "title": "Victory APIs",
+        "description": "API documentation",
+        "version": "1.0.0"
+    },
+    "basePath": "/",  # base path for endpoints
+}
+
+swagger_config = {
+    "headers": [],
+    "specs": [
+        {
+            "endpoint": "apispec_1",
+            "route": "/apispec_1.json",
+            "rule_filter": lambda rule: True,   # include all endpoints
+            "model_filter": lambda tag: True,   # include all models
+        }
+    ],
+    "static_url_path": "/flasgger_static",
+    "swagger_ui": True,
+    "specs_route": "/apidocs/"
+}
 
 
 def create_app(config_class=Config):
@@ -15,5 +42,8 @@ def create_app(config_class=Config):
 
     # Register blueprints
     register_blueprints(app)
+
+    # Adding Swagger
+    Swagger(app, template=swagger_template, config=swagger_config)
 
     return app
